b6da0d617bfa2389345d3be4874542472c663d75
HarshiniRavichandran18 <harshini4069@gmail.com>
2026-01-28 16:01:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.weather_routes import weather_bp
 
 app = Flask(__name__)
+
+# Register API routes
 app.register_blueprint(weather_bp)
 
-# Render uses this
+# Frontend route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Required for Render deployment
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
